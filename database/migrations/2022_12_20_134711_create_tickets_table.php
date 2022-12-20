9bfba7d3cd02b51be329b12ae8173b3a01e6a9b0
Matteogatto94 <cosenza94@live.it>
2022-12-20 15:01:35 +0100

    create a table in database

--- database/migrations/2022_12_20_134711_create_tickets_table.php
+++ database/migrations/2022_12_20_134711_create_tickets_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->string('agency', 30);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->float('departure_time', 4, 2);
+            $table->float('arrival_time', 4, 2);
+            $table->string('reference_number', 12);
+            $table->tinyInteger('carriages')->unsigned()->nullable();
+            $table->boolean('is_available')->default(1);
+            $table->boolean('is_deleted')->default(1);
             $table->timestamps();
         });
     }
